5fb92bfdc225dfc9d270ac47e632dfd1d52cdd41
Eva <42466980+po5@users.noreply.github.com>
2019-10-14 06:31:06 +0200

    Proper config handling

--- groupwatch_sync.lua
+++ groupwatch_sync.lua
@@ -5,19 +5,28 @@
 -- This script speeds up or slows down videos to
 -- get back in sync with a group watch.
 -- Define a start time with Shift+K and sync with K.
-local allow_slowdowns = false -- if true, slows down playback instead of pausing when ahead
-local speed_increase = .2
-local speed_decrease = .2
-local min_speed = .25
-local max_speed = 2.5
 
+local options = {
+    -- If true, slows down playback instead of pausing when ahead
+    allow_slowdowns = false,
+	
+    -- Playback speed modifier when syncing, applied once every second until cap is reached
+    speed_increase = .2,
+    speed_decrease = .2,
 
------------------------
+    -- Playback speed cap
+    min_speed = .25,
+    max_speed = 2.5,
+}
+
 local mp = require 'mp'
 local start = nil
 local syncing = false
 local pausing = false
 local last_correction = 0
+
+mp.options = require 'mp.options'
+mp.options.read_options(options, "groupwatch_sync")
 
 local function groupwatch_reset()
     if syncing then
@@ -52,7 +61,7 @@ local function groupwatch_start(from)
     mp.osd_message("[groupwatch_sync] start time set")
 end
 
-local function groupwatch_start_from()
+local function groupwatch_start_here()
     groupwatch_start(mp.get_property_number("time-pos"))
 end
 
@@ -95,27 +104,27 @@ local function groupwatch_observe()
         pausing = false
         return true
     end
-    local speed_correction = speed_increase
+    local speed_correction = options.speed_increase
     if local_pos >= groupwatch_pos + .8 then
-        if not allow_slowdowns then
+        if not options.allow_slowdowns then
             mp.osd_message("[groupwatch_sync] syncing...", local_pos - groupwatch_pos)
             mp.set_property_bool("pause", true)
             pausing = true
             return mp.add_timeout(local_pos - groupwatch_pos, groupwatch_unpause)
         end
-        speed_correction = -speed_decrease
+        speed_correction = -options.speed_decrease
     end
     new_correction = math.ceil(local_pos)
     if new_correction ~= last_correction then
         last_correction = new_correction
-        local new_speed = math.max(min_speed, math.min(mp.get_property_number("speed") + speed_correction, max_speed))
+        local new_speed = math.max(options.min_speed, math.min(mp.get_property_number("speed") + speed_correction, options.max_speed))
         mp.set_property("speed", new_speed)
     end
     mp.osd_message("[groupwatch_sync] syncing...")
 end
 
 mp.register_event("start-file", groupwatch_reset)
-mp.add_forced_key_binding("Ctrl+k", "groupwatch_start_from", groupwatch_start_from)
+mp.add_forced_key_binding("Ctrl+k", "groupwatch_start_here", groupwatch_start_here)
 mp.add_forced_key_binding("K", "groupwatch_start", groupwatch_start)
 mp.add_forced_key_binding("k", "groupwatch_sync", groupwatch_sync)
 mp.observe_property("time-pos", "native", groupwatch_observe)
